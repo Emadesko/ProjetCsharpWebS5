97b89645e3820be746fd2813fc805d1eaf10293d
Emadesko <154944503+Emadesko@users.noreply.github.com>
2024-11-13 10:42:39 +0000

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetCsharpWebS5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
